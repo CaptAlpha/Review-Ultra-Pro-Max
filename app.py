e6307d3f1a11c686f430858e514bf3de4f4c4f86
CaptAlpha <arhitbose.tagore2020@vitstudent.ac.in>
2022-06-06 13:57:08 +0530

    Added Scraper

--- app.py
+++ app.py
@@ -3,16 +3,41 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 from flask import session as login_session
 from flask import make_response
 from sqlalchemy import create_engine
+from scraperbs import getdata, html_code
 
 app = Flask(__name__)
 
 #First Page Render - Home Page
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def home():
+    link = ""
     #Request for link from index.html
     if request.method == 'POST':
         link = request.form['link']
-    return render_template('home.html', link=link)
+        #redirect to scrape route
+        return redirect(url_for('scrape', link=link))
+    return render_template('index.html')
 
+# Web Scraping route
+@app.route('/scrape', methods=['GET', 'POST'])
+def scrape():
+    #return to scrape.html 
+    link = request.args.get('link')
+    #Go to the product reviews and scrape the data
+    data = html_code(link)
+    #pass the soup object to get_data function
+    
+
+
+    return render_template('scrape.html', link=link, data=data)
+
+    
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(debug=True)
 
---
+++ scraperbs.py
@@ -0,0 +1,27 @@
+
+# import module
+import requests
+from bs4 import BeautifulSoup
+  
+HEADERS = ({'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) \
+            Chrome/90.0.4430.212 Safari/537.36',
+            'Accept-Language': 'en-US, en;q=0.5'})
+  
+# user define function
+# Scrape the data
+def getdata(url):
+    r = requests.get(url, headers=HEADERS)
+    return r.text
+  
+  
+def html_code(url):
+  
+    # pass the url
+    # into getdata function
+    htmldata = getdata(url)
+    soup = BeautifulSoup(htmldata, 'html.parser')
+  
+    # display html code
+    return (soup)
